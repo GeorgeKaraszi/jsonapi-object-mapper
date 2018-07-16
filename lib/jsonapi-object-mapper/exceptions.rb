ff1da4cafa339f21c324cae4c43458fa2bc2f84a
George Karaszi <georgekaraszi@gmail.com>
2018-07-15 22:52:30 -0400

    Implement standard exception messages

--- lib/jsonapi-object-mapper/exceptions.rb
+++ lib/jsonapi-object-mapper/exceptions.rb
@@ -2,8 +2,23 @@
 
 module JsonAPIObjectMapper
   class InvalidResource < StandardError
+    def initialize(msg = nil)
+      msg ||= "The deserializer class must be an inherited `JsonAPIObjectMapper::Deserialize::Resource` klass"
+      super
+    end
   end
 
   class InvalidParser < StandardError
+    def initialize(msg = nil)
+      msg ||= "Must provide a parsed `JsonAPIObjectMapper::Parser::Document` klass-document"
+      super
+    end
+  end
+
+  class InvalidEmbedKlass < StandardError
+    def initialize(msg = nil)
+      msg ||= "The `embed_with: ...` option, must be a inherited `JsonAPIObjectMapper::Deserialize::Resource` klass"
+      super
+    end
   end
 end
